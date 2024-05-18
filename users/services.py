bd020d61ef91d0bd33ad7feaf304287068253091
ekso <baroniusokay@gmail.com>
2024-05-19 01:07:07 +0200

    developing phase 4

--- users/services.py
+++ users/services.py
@@ -1,4 +1,15 @@
+import os
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+from passlib.context import CryptContext
+
 from .db import users
+from .exceptions import UserNotFound, EmailAlreadyExists
+from .models import User
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 
 class UserService:
@@ -9,27 +20,56 @@ class UserService:
     @staticmethod
     def get_user(user_id):
         user = next((user for user in users if user.id == user_id), None)
+        print(user.password)
         if not user:
-            raise UserService.UserNotFound()
+            raise UserNotFound(f"User with ID {user_id} not found.")
         return user
 
     @staticmethod
     def create_user(data):
-        new_user = {'id': max(user.id for user in users) + 1, **data}
+        if any(u.email == data.get('email') for u in users):
+            raise EmailAlreadyExists(f"Email {data.get('email')} is already in use.")
+
+        new_user = User(
+            id=max((user.id for user in users), default=0) + 1,
+            email=data.get('email'),
+            password=pwd_context.hash(data.get('password')),
+            is_admin=data.get('is_admin') == 'true'
+        )
         users.append(new_user)
         return new_user
 
     @staticmethod
     def update_user(user_id, update_data):
         user = UserService.get_user(user_id)
-        user.update(update_data)
+        if any(u.email == update_data.get('email') and u.id != user_id for u in users):
+            raise EmailAlreadyExists(f"Email {update_data.get('email')} is already in use.")
+        if user:
+            user.email = update_data.get('email', user.email)
+            user.password = pwd_context.hash(update_data.get('password', user.password))
+            user.is_admin = update_data.get('is_admin', user.is_admin) == 'true'
         return user
 
     @staticmethod
     def delete_user(user_id):
         global users
-        print(users)
         users = [user for user in users if user.id != user_id]
 
-    class UserNotFound(Exception):
-        pass
+
+class MailService:
+    @staticmethod
+    def send_email(recipient_email):
+        # Create the message
+        message = MIMEMultipart()
+        message['From'] = os.getenv('MAIL_FROM_NAME') + " <" + os.getenv('MAIL_FROM') + ">"
+        message['To'] = recipient_email
+        message['Subject'] = 'Підтвердження створення акаунту'
+
+        body = f"Привіт, ваш акаунт було успішно створено!"
+        message.attach(MIMEText(body, 'html'))
+
+        # Connect to the SMTP server and send the email
+        with smtplib.SMTP(os.getenv('MAIL_SERVER'), int(os.getenv('MAIL_PORT'))) as server:
+            server.starttls()
+            server.login(os.getenv('MAIL_USERNAME'), os.getenv('MAIL_PASSWORD'))
+            server.send_message(message)
--- users/views.py
+++ users/views.py
@@ -1,6 +1,9 @@
+from threading import Thread
+
 from django.shortcuts import render, redirect
 
-from .services import UserService
+from .exceptions import EmailAlreadyExists, UserNotFound
+from .services import UserService, MailService
 
 
 def list_users(request):
@@ -12,22 +15,31 @@ def user_detail(request, user_id):
     try:
         user = UserService.get_user(user_id)
         return render(request, 'users/detail.html', {'user': user})
-    except UserService.UserNotFound:
+    except UserNotFound:
         return render(request, '404.html', status=404)
 
 
 def user_create(request):
     if request.method == 'POST':
-        new_user = UserService.create_user(request.POST)
-        return render(request, 'users/detail.html', {'user': new_user})
+        try:
+            new_user = UserService.create_user(request.POST)
+            # Send email in a separate thread
+            Thread(target=MailService.send_email, args=new_user.email).start()
+            return render(request, 'users/detail.html', {'user': new_user})
+        except EmailAlreadyExists as e:
+            return render(request, 'users/create.html', {'error': str(e), 'form_data': request.POST})
     return render(request, 'users/create.html')
 
 
 def user_update(request, user_id):
+    user = UserService.get_user(user_id)
     if request.method == 'POST':
-        updated_user = UserService.update_user(user_id, request.POST)
-        return render(request, 'users/detail.html', {'user': updated_user})
-    return render(request, 'users/update.html', {'user': UserService.get_user(user_id)})
+        try:
+            updated_user = UserService.update_user(user_id, request.POST)
+            return render(request, 'users/detail.html', {'user': updated_user})
+        except EmailAlreadyExists as e:
+            return render(request, 'users/update.html', {'error': str(e), 'user': user, 'form_data': request.POST})
+    return render(request, 'users/update.html', {'user': user})
 
 
 def user_delete(request, user_id):
